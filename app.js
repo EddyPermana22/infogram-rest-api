a887d1238d0ec5af8fc3dd984723f5d122139d3e
Eddy Permana <me.eddypermana@gmail.com>
2021-01-20 14:36:21 +0700

    complete the login and register method

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const express = require("express");
 const cors = require("cors");
 
 const Router = require("./routers");
+const errorHandler = require("./middlewares/errorHandler");
 
 const port = process.env.PORT;
 
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(Router);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`server ready, listening on port ${port}`);
--- controllers/user.js
+++ controllers/user.js
@@ -1,24 +1,77 @@
 "use strict";
 
+const modelUser = require("../models").User;
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const md5 = require("md5");
+
 class UserController {
-  static register = async (req, res, next) => {
-    try {
-      res.status(200).json({
-        message: `register success!`,
-      });
-    } catch (error) {
-      next(error);
+  static register = (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (!email) {
+      throw new Error("Email Required!");
     }
+
+    if (!password) {
+      throw new Error("Password Required!");
+    }
+
+    modelUser
+      .count({
+        where: {
+          email: email.toLowerCase().trim(),
+        },
+      })
+      .then((userCount) => {
+        if (userCount > 0) {
+          throw new Error(
+            `email ${email} has been registred, please try register another email!`
+          );
+        } else {
+          return modelUser.create({
+            email: email.toLowerCase().trim(),
+            password: password,
+          });
+        }
+      })
+      .then((user) => {
+        res.status(201).json({
+          message: `Registration Success !`,
+          user,
+        });
+      })
+      .catch(next);
   };
 
-  static login = async (req, res, next) => {
-    try {
-      res.status(200).json({
-        message: `login success!`,
-      });
-    } catch (error) {
-      next(error);
-    }
+  static login = (req, res, next) => {
+    modelUser
+      .findOne({
+        where: {
+          email: req.body.email.toLowerCase().trim(),
+        },
+      })
+      .then((user) => {
+        if (
+          !user ||
+          !bcrypt.compareSync(req.body.password || "", user.password)
+        ) {
+          next({
+            message: `Invalid email / Password !`,
+          });
+        } else {
+          const payload = {
+            userID: user.id,
+            checker: md5(user.updatedAt),
+          };
+          const access_token = jwt.sign(payload, process.env.JWT_SECRET_KEY);
+          res.status(200).json({
+            message: `signin success`,
+            access_token,
+          });
+        }
+      })
+      .catch(next);
   };
 }
 
