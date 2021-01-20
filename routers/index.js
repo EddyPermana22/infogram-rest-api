64eb611bfd13523317a2235bb67c7e6b620c1b2f
Eddy Permana <me.eddypermana@gmail.com>
2021-01-20 10:29:15 +0700

    add user router to main router

--- routers/index.js
+++ routers/index.js
@@ -2,4 +2,14 @@
 
 const Router = require("express").Router();
 
+const UserRouter = require("./user");
+
+Router.get("/", (req, res, next) => {
+  res.status(200).json({
+    message: `API Server Ready!`,
+  });
+});
+
+Router.use("/users", UserRouter);
+
 module.exports = Router;
