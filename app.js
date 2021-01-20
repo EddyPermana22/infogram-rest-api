efad8bc20a6bb7e7c13119329cf33e7cdc21f4e6
Eddy Permana <me.eddypermana@gmail.com>
2021-01-20 10:30:04 +0700

    add main router to app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 "use strict";
 
-require("dotenv").config()
+require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+
+const Router = require("./routers");
 
 const port = process.env.PORT;
 
@@ -12,6 +14,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(Router);
 
 app.listen(port, () => {
   console.log(`server ready, listening on port ${port}`);
